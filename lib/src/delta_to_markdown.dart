b701db87341cb5300a714d2674a5110444428dd1
Fabian Terhorst <fabian.terhorst@gmail.com>
2022-01-04 15:38:57 +0100

    fix unknown type crash

--- lib/src/delta_to_markdown.dart
+++ lib/src/delta_to_markdown.dart
@@ -176,6 +176,7 @@ class DeltaToMarkdown extends Converter<Delta, String>
 
   final Map<String, EmbedToMarkdown> _embedHandlers = {
     BlockEmbed.imageType: (embed, out) => out.write('![](${embed.value.data})'),
+    BlockEmbed.videoType: (embed, out) => out.write('![](${embed.value.data})'),
     BlockEmbed.horizontalRuleType: (embed, out) {
       // adds new line after it
       // make --- separated so it doesn't get rendered as header
